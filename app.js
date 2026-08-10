70b6c5f26d4c0ab4391414e057706250d0ea0216
kizito67 <oluigbokzito@gmail.com>
2026-08-10 13:20:35 -0700

    Enable CORS for frontend integration

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const morgan = require('morgan');
-require('dotenv').config();
+const morgan = require("morgan");
+require("dotenv").config();
 
+const connectDB = require("./src/config/db");
 
-const connectDB = require('./src/config/db');
-
-
-app.use(morgan('dev')); 
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to BTBS...');
+app.get("/", (req, res) => {
+  res.send("Welcome to BTBS...");
 });
 
-
-
-app.use('/api/auth', require('./src/routes/auth.routes'));
-app.use('/api/routes', require('./src/routes/route.routes'));
-app.use('/api/confirmations', require('./src/routes/confirmation.routes'));
-app.use('/api/safety-points', require('./src/routes/safetyPoint.routes'));
-
-
-
+app.use("/api/auth", require("./src/routes/auth.routes"));
+app.use("/api/routes", require("./src/routes/route.routes"));
+app.use("/api/confirmations", require("./src/routes/confirmation.routes"));
+app.use("/api/safety-points", require("./src/routes/safetyPoint.routes"));
 
 app.listen(PORT, () => {
-    connectDB();
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
